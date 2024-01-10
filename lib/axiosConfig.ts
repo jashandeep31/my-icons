1f8c8057faa48a00d7d35642b3f5902edb401ee5
jashandeep <jashandeep1659@gmail.com>
2024-01-11 00:02:47 +0530

    fix: console removed from axios

--- lib/axiosConfig.ts
+++ lib/axiosConfig.ts
@@ -1,4 +1,3 @@
 // export const baseUrl = "https://my-icons-bice.vercel.app/api";
 // export const baseUrl = "https://my-icons-bice.vercel.app/api" as string;
 export const baseUrl = process.env.NEXT_PUBLIC_SELF_URL;
-console.log("🚀 ~ baseUrl:", baseUrl);
