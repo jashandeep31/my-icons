03f735223459142fdefaf0b866e2b2c7a603a297
jashandeep <jashandeep1659@gmail.com>
2024-01-10 10:27:22 +0530

    fix: added static value temp fix

--- lib/axiosConfig.ts
+++ lib/axiosConfig.ts
@@ -1,1 +1,2 @@
-export const baseUrl = process.env.SELF_URL as string;
+export const baseUrl = "https://my-icons-bice.vercel.app/api";
+// export const baseUrl = "https://my-icons-bice.vercel.app/api" as string;
