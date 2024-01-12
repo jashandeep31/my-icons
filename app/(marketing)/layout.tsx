26861998f35109a062b04ffc279b76dc125b2f74
jashandeep <jashandeep1659@gmail.com>
2024-01-12 21:22:46 +0530

    fix: removed the support us button

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -3,18 +3,6 @@ import Navbar from "@/components/navbar";
 import React from "react";
 import LayoutClient from "./layout.client";
 
-const button = `
-<script src='https://storage.ko-fi.com/cdn/scripts/overlay-widget.js'></script>
-
-<script>
-  kofiWidgetOverlay.draw('myicons', {
-    'type': 'floating-chat',
-    'floating-chat.donateButton.text': 'Support Us',
-    'floating-chat.donateButton.background-color': '#00b9fe',
-    'floating-chat.donateButton.text-color': '#fff',
-  });
-</script>`;
-
 export default async function layout({
   children,
 }: {
@@ -27,7 +15,6 @@ export default async function layout({
       <header className="sticky top-0">
         <Navbar />
       </header>
-      <div dangerouslySetInnerHTML={{ __html: button }} />
 
       <main className="flex-1 mb-6">{children}</main>
       <Footer />
