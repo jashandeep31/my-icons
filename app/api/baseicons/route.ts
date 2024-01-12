759e72126e5cb6c06843263d41cad26ce4a6d8c2
jashandeep <jashandeep1659@gmail.com>
2024-01-12 18:27:46 +0530

    fix: removed sharp lib

--- app/api/baseicons/route.ts
+++ app/api/baseicons/route.ts
@@ -2,6 +2,8 @@ import catchAsync from "@/lib/catchAsync";
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
+
+export const dynamic = "force-dynamic";
 export const GET = catchAsync(async (req: Request) => {
   // TODO: only for approved and add flters like windows macos
   const baseIcons = await db.baseIcon.findMany();
