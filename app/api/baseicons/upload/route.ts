0baad918527192e6be0da6d939d690dd4656f30e
jashandeep <jashandeep1659@gmail.com>
2024-01-12 18:20:59 +0530

    fix: little change in baseicon api

--- app/api/baseicons/upload/route.ts
+++ app/api/baseicons/upload/route.ts
@@ -38,14 +38,14 @@ export const POST = catchAsync(async (req: Request) => {
 
   const url = await uploadToS3(
     "baseicons",
-    slugify(validatedFormData.name, {
-      replacement: "-", // replace spaces with replacement character, defaults to `-`
-      remove: undefined, // remove characters that match regex, defaults to `undefined`
-      lower: true, // convert to lower case, defaults to `false`
-      strict: true, // strip special characters except replacement, defaults to `false`
-      locale: "vi", // language code of the locale to use
+    `${slugify(validatedFormData.name, {
+      replacement: "-",
+      remove: undefined,
+      lower: true,
+      strict: true,
+      locale: "vi",
       trim: true,
-    }),
+    })}.png`,
     icon
   );
 
