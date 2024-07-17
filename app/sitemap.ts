87abed654c2d87abc4218dc5e8227260065e1c2b
jashandeep <jashandeep16592gmail.com>
2024-07-17 11:35:48 +0530

    update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,42 +1,42 @@
-// import { db } from "@/lib/db"
-// import { MetadataRoute } from "next"
+import { db } from "@/lib/db"
+import { MetadataRoute } from "next"
 
-// export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-//   const URL = process.env.NEXT_PUBLIC_SELF ?? ""
-//   const map = [
-//     {
-//       url: `${URL}`,
-//       lastModified: new Date(),
-//       priority: 1,
-//     },
-//     {
-//       url: `${URL}/create`,
-//       lastModified: new Date(),
-//       priority: 1,
-//     },
-//     { url: `${URL}/icons`, lastModified: new Date(), priority: 0.5 },
-//     {
-//       url: `${URL}/terms-and-conditions`,
-//       lastModified: new Date(),
-//       priority: 0.5,
-//     },
-//     { url: `${URL}/privacy-policy`, lastModified: new Date(), priority: 0.5 },
-//     { url: `${URL}/privacy-policy`, lastModified: new Date(), priority: 0.5 },
-//   ]
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const URL = process.env.NEXT_PUBLIC_SELF ?? ""
+  const map = [
+    {
+      url: `${URL}`,
+      lastModified: new Date(),
+      priority: 1,
+    },
+    {
+      url: `${URL}/create`,
+      lastModified: new Date(),
+      priority: 1,
+    },
+    { url: `${URL}/icons`, lastModified: new Date(), priority: 0.5 },
+    {
+      url: `${URL}/terms-and-conditions`,
+      lastModified: new Date(),
+      priority: 0.5,
+    },
+    { url: `${URL}/privacy-policy`, lastModified: new Date(), priority: 0.5 },
+    { url: `${URL}/privacy-policy`, lastModified: new Date(), priority: 0.5 },
+  ]
 
-//   const icons = await db.icon.findMany({
-//     orderBy: {
-//       createdAt: "desc",
-//     },
-//   })
+  // const icons = await db.icon.findMany({
+  //   orderBy: {
+  //     createdAt: "desc",
+  //   },
+  // })
 
-//   icons.map((icon, index: number) => {
-//     map.push({
-//       url: `${URL}/icon/${icon.id}`,
-//       lastModified: new Date(),
-//       priority: 0.7,
-//     })
-//   })
+  // icons.map((icon, index: number) => {
+  //   map.push({
+  //     url: `${URL}/icon/${icon.id}`,
+  //     lastModified: new Date(),
+  //     priority: 0.7,
+  //   })
+  // })
 
-//   return map
-// }
+  return map
+}
